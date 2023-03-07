b98bd0cb85ef4b86c34c72c1d3ca73e0939aeb43
Akmal Rafi Diara Putra <49126626+akmalrafidiara@users.noreply.github.com>
2023-03-07 16:17:51 +0700

    upload file

--- tambah-data.php
+++ tambah-data.php
@@ -2,6 +2,26 @@
 
 <?php
 if (isset($_POST['submit'])) {
+
+  $file_name = $_FILES['image']['name'];
+  $file_size = $_FILES['image']['size'];
+  $file_tmp = $_FILES['image']['tmp_name'];
+  $file_type = $_FILES['image']['type'];
+  $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+
+  $extensions = array("jpeg", "jpg", "png", "gif");
+
+  if (in_array($file_ext, $extensions) === false) {
+    echo "<script>alert('Extension not allowed, please choose a JPEG or PNG file.')</script>";
+  }
+
+  if ($file_size > 2097152) {
+    echo "<script>alert('File size must be excately 2 MB')</script>";
+  }
+
+  $file_name = time() . '_' . $file_name;
+  move_uploaded_file($file_tmp, "upload/" . $file_name);
+  $file_name = "upload/" . $file_name;
   $sql = "INSERT INTO product (name, country, description, image, stock, price)
           VALUES
           (:name, :country, :description, :image, :stock, :price)";
@@ -9,7 +29,7 @@ if (isset($_POST['submit'])) {
   $stmt->bindParam(':name', $_POST['name']);
   $stmt->bindParam(':country', $_POST['country']);
   $stmt->bindParam(':description', $_POST['description']);
-  $stmt->bindParam(':image', $_POST['image']);
+  $stmt->bindParam(':image', $file_name);
   $stmt->bindParam(':stock', $_POST['stock']);
   $stmt->bindParam(':price', $_POST['price']);
   $stmt->execute();
@@ -32,11 +52,14 @@ if (isset($_POST['submit'])) {
 </head>
 
 <body>
-  <form action="" method="post">
+  <form action="" method="POST" enctype="multipart/form-data">
     <input type="text" name="name" placeholder="name" required><br>
     <input type="text" name="country" placeholder="country" required><br>
     <textarea name="description" cols="30" rows="10" placeholder="description"></textarea><br>
-    <input type="text" name="image" placeholder="image" required><br>
+    <div>
+      <label>Image</label>
+      <input type="file" name="image" required><br>
+    </div>
     <input type="number" name="stock" placeholder="stock" required><br>
     <input type="number" name="price" placeholder="price" required><br>
     <button type="submit" name="submit">Save</button>
